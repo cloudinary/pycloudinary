c126fa015196376e2992b90b22bc300660e2ad4e
Nadav Soferman <nadav.soferman@gmail.com>
2012-07-08 09:16:10 +0300

    Configuration changes for packaging

--- setup.py
+++ setup.py
@@ -1,25 +1,21 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.2'
+version = '0.2.0'
 
 setup(name='pycloudinary',
       version=version,
       description="Python interface to Cloudinary",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='cloudinary image upload cdn',
-      author='Tal Lev-Ami',
-      author_email='tal.levami@cloudinary.com',
+      long_description='',
+      classifiers=[], 
+      keywords='cloudinary image upload transformation cdn',
+      author='Cloudinary',
+      author_email='info@cloudinary.com',
       url='http://cloudinary.com',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       test_suite="tests",                          
-      install_requires=["poster" ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+      install_requires=["poster"]
       )
