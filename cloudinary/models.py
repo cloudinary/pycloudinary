19717ae8d44b4c6ca02b04fbce1beabd1cef8361
Tal Lev-Ami <tal.levami@gmail.com>
2012-10-30 13:13:13 +0200

    Issue 8 - error in serialization

--- cloudinary/models.py
+++ cloudinary/models.py
@@ -20,7 +20,7 @@ class CloudinaryField(models.Field):
 
   def value_to_string(self, obj):
     value = self._get_val_from_obj(obj)
-    return self.get_db_prep_value(value)
+    return self.get_prep_value(value)
 
   def to_python(self, value):
     if isinstance(value, CloudinaryImage):
