c1b2f6b6b740fc0bda994e92a6893717574f8e09
Tal Lev-Ami <tal.levami@gmail.com>
2013-02-05 12:56:35 +0200

    Fix whitespace issues after merge

--- cloudinary/templatetags/cloudinary.py
+++ cloudinary/templatetags/cloudinary.py
@@ -8,24 +8,24 @@ register = template.Library()
 
 @register.simple_tag(name='cloudinary')
 def cloudinary_tag(image, **options):
-  if not isinstance(image, CloudinaryImage):
-    image = CloudinaryImage(image)
-  return image.image(**options)
+    if not isinstance(image, CloudinaryImage):
+        image = CloudinaryImage(image)
+    return image.image(**options)
 
 @register.inclusion_tag('cloudinary_direct_upload.html')
 def cloudinary_direct_upload(callback_url, **options):
-  params = uploader.build_upload_params(callback=callback_url, **options)  
-  params["signature"] = utils.api_sign_request(params, cloudinary.config().api_secret)  
-  params["api_key"] = cloudinary.config().api_key
-  
-  api_url = utils.cloudinary_api_url("upload", resource_type=options.get("resource_type", "image"), upload_prefix=options.get("upload_prefix"))
+    params = uploader.build_upload_params(callback=callback_url, **options)    
+    params["signature"] = utils.api_sign_request(params, cloudinary.config().api_secret)    
+    params["api_key"] = cloudinary.config().api_key
+    
+    api_url = utils.cloudinary_api_url("upload", resource_type=options.get("resource_type", "image"), upload_prefix=options.get("upload_prefix"))
 
-  for k, v in params.items():
-    if not v:
-      del params[k]
+    for k, v in params.items():
+        if not v:
+            del params[k]
 
-  return {"params": params, "url": api_url}
+    return {"params": params, "url": api_url}
 
 @register.inclusion_tag('cloudinary_includes.html')
 def cloudinary_includes(processing=False):
-  return {"processing": processing}
+    return {"processing": processing}
