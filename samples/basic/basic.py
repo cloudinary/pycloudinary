0f8da81304ae256495a1c431717addd9e1abcce3
Alon Hammerman <the.alonso@gmail.com>
2013-02-19 23:04:53 +0200

    samples - create basic sample

---
+++ samples/basic/basic.py
@@ -0,0 +1,77 @@
+import cloudinary
+import cloudinary.uploader
+import os, sys
+
+# config
+os.chdir(os.path.join(os.path.dirname(sys.argv[0]), '.'))
+if os.path.exists('settings.py'):
+    execfile('settings.py')
+
+# uploads data
+SAMPLE_PATHS = dict(
+    pizza = "pizza.jpg",
+    lake = "lake.jpg",
+    couple = "http://res.cloudinary.com/demo/image/upload/couple.jpg",
+)
+
+DEFAULT_OPTIONS = dict(tags = "python_sample_basic")
+DEFAULT_THUMBS = dict(width = 200, height = 150)
+EAGER_OPTIONS = dict(DEFAULT_THUMBS, crop = "scale")
+
+# <title>, <filepath>, <upload params>, <display params>
+UPLOADS = [
+    ["unnamed local, crop - fill", SAMPLE_PATHS["pizza"], {}, dict(crop = "fill")],
+    ["named local, crop - fit", SAMPLE_PATHS["pizza"], dict(public_id = "named"), dict(crop = "fit")],
+    ["local with eager tranformations", SAMPLE_PATHS["lake"], 
+        dict(public_id = "eager", eager = EAGER_OPTIONS), EAGER_OPTIONS
+    ],
+
+    ["fetch remotely + crop gravity faces", SAMPLE_PATHS["couple"], {}, 
+        dict(crop = "thumb", gravity = "faces")],
+    ["fetch remotely with effects", SAMPLE_PATHS["couple"], dict(public_id = "transformed",
+        width = 500, height = 500, crop = "fit", effect = "saturation:-70"),
+        dict(crop = "fill", gravity = "face", radius = 10),
+    ],
+]
+
+# upload code
+""" Uploads specified files and returns responses """
+def do_upload(filename, params):
+    return cloudinary.uploader.upload(filename, **dict(DEFAULT_OPTIONS.items() + params.items()))
+
+def show_response(title, response, url):
+    print "done -- public_id: %(public_id)s (%(format)s) size: %(width)dx%(height)d" % (
+        dict(response, title=title))
+    print "  " + url
+
+def upload(uploads):
+    for index, item in enumerate(uploads):
+        title, filename, upload_params, display_params = item
+        print "uploading " + title + "..."
+        response = do_upload(filename, upload_params)
+        url = cloudinary.utils.cloudinary_url(response["public_id"], **dict(
+            [("format", response["format"])] + DEFAULT_THUMBS.items() + display_params.items()
+        ))[0]
+        show_response(title, response, url)
+
+def delete():
+    pass
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'upload': upload(UPLOADS)
+    if sys.argv[1] == 'delete': delete()
+    if 'test' in sys.argv:
+        print os.getcwd(), sys.argv, __package__, __name__
+
+"""
+{u'bytes': 53696,
+ u'format': u'jpg',
+ u'height': 360,
+ u'public_id': u'sidjkjtpmnysrqa92xfj',
+ u'resource_type': u'image',
+ u'secure_url': u'https://d3jpl91pxevbkh.cloudfront.net/ndividev/image/upload/v1361304062/sidjkjtpmnysrqa92xfj.jpg',
+ u'signature': u'b1a528c55097e357d50cdb7efde8cec86241a105',
+ u'url': u'http://res.cloudinary.com/ndividev/image/upload/v1361304062/sidjkjtpmnysrqa92xfj.jpg',
+ u'version': 1361304062,
+ u'width': 480}
+"""
