69d2ed8458031f7dedf3d19e3d3f7be009519bc7
Tal Lev-Ami <tal.levami@gmail.com>
2015-04-07 14:06:10 +0300

    Increment to v1.1.0 - Support saving non-image resource types in CloudinaryField. Support storing type information in database. Support video tag and video thumbnail generation. Support video transformation parameters. Support eager_notification_url and eager_async in explicit. Support ftp urls in upload. upload large enhancements. Python3 compatibility fixes.

--- cloudinary/__init__.py
+++ cloudinary/__init__.py
@@ -10,7 +10,7 @@ AKAMAI_SHARED_CDN = "res.cloudinary.com"
 SHARED_CDN = AKAMAI_SHARED_CDN
 CL_BLANK = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
 
-VERSION = "1.0.21"
+VERSION = "1.1.0"
 USER_AGENT = "cld-python-" + VERSION
 
 from cloudinary import utils
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.0.21'
+version = '1.1.0'
 
 setup(name='cloudinary',
       version=version,
