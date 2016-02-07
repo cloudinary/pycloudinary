3ea692f3f881158f8a46a32690008e1761f59dc8
Amir Tocker <tocker.signup@gmail.com>
2016-02-07 12:09:54 +0200

    Replace `ARCHIVE_TAG` with `TEST_TAG`

--- tests/archive_test.py
+++ tests/archive_test.py
@@ -6,29 +6,30 @@ import tempfile
 import os
 import zipfile
 
-ARCHIVE_TAG = "archive_tag_" + utils.now()
+TEST_TAG = "pycloudinary_test"
+
 class ArchiveTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cloudinary.reset_config()
-        uploader.upload("tests/logo.png", tags = [ARCHIVE_TAG])
-        uploader.upload("tests/logo.png", tags = [ARCHIVE_TAG], transformation=dict(width=10))
+        uploader.upload("tests/logo.png", tags = [TEST_TAG])
+        uploader.upload("tests/logo.png", tags = [TEST_TAG], transformation=dict(width=10))
     
     @classmethod
     def tearDownClass(cls):
-        api.delete_resources_by_tag(ARCHIVE_TAG)
+        api.delete_resources_by_tag(TEST_TAG)
 
     @unittest.skipUnless(cloudinary.config().api_secret, "requires api_key/api_secret")
     def test_create_archive(self):
         """should successfully generate an archive"""
-        result = uploader.create_archive(tags = [ARCHIVE_TAG])
+        result = uploader.create_archive(tags = [TEST_TAG])
         self.assertEqual(2, result.get("file_count"))
-        result = uploader.create_zip(tags = [ARCHIVE_TAG], transformations = [{"width": 0.5},{"width": 2.0}])
+        result = uploader.create_zip(tags = [TEST_TAG], transformations = [{"width": 0.5},{"width": 2.0}])
         self.assertEqual(4, result.get("file_count"))
 
     @unittest.skipUnless(cloudinary.config().api_secret, "requires api_key/api_secret")
     def test_archive_url(self):
-        result = utils.download_zip_url(tags = [ARCHIVE_TAG], transformations = [{"width": 0.5},{"width": 2.0}])
+        result = utils.download_zip_url(tags = [TEST_TAG], transformations = [{"width": 0.5},{"width": 2.0}])
         response = urllib2.urlopen(result)
         temp_file = tempfile.NamedTemporaryFile()
         temp_file_name = temp_file.name
