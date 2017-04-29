6da052ca0eb89b6358eeddb66a0e93988e252e56
Amir Tocker <amir.tocker@cloudinary.com>
2017-04-29 23:39:32 +0300

    Add `logger` to cloudinary.

--- cloudinary/__init__.py
+++ cloudinary/__init__.py
@@ -1,5 +1,12 @@
 from __future__ import absolute_import
 
+import logging
+logger = logging.getLogger("Cloudinary")
+ch = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 import os
 import re
 
