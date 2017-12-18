fc77e70c4d1aa229bc91580d28fab5aaab4d8a29
Amir Tocker <amir.tocker@cloudinary.com>
2017-12-18 17:11:30 +0200

    Add test helper methods

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,16 +1,40 @@
 import os
 import random
 
+import re
+
 SUFFIX = os.environ.get('TRAVIS_JOB_ID') or random.randint(10000, 99999)
 TEST_IMAGE = "tests/logo.png"
 TEST_TAG = "pycloudinary_test"
 UNIQUE_TAG = "{0}_{1}".format(TEST_TAG, SUFFIX)
 
-def get_method(args):
-    return args[0]
+
+def get_method(mocker):
+    return mocker.call_args[0][0]
+
 
 def get_uri(args):
     return args[1]
 
+
 def get_params(args):
     return args[2] or {}
+
+
+def get_param(mocker, name):
+    args, kargs = mocker.call_args
+    params = get_params(args)
+    return params.get(name)
+
+
+def get_list_param(mocker, name):
+    """
+    Return a list of values for the given param name
+    :param mocker: 
+    :param name: 
+    :return: 
+    """
+    args, kargs = mocker.call_args
+    params = get_params(args)
+    reg = re.compile(r'{}\[\d*\]'.format(name))
+    return [params[key] for key in params.keys() if reg.match(key)]
