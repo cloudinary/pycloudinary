13f721eb56b83d62dde75fa9d08ea149cd75b495
Constantine Nathanson <constantine@cloudinary.com>
2018-01-17 15:44:44 +0200

    Fix `test_download_zip_url_options` unit test with custom upload prefix

--- tests/archive_test.py
+++ tests/archive_test.py
@@ -91,7 +91,8 @@ class ArchiveTest(unittest.TestCase):
     @unittest.skipUnless(cloudinary.config().api_secret, "requires api_key/api_secret")
     def test_download_zip_url_options(self):
         result = utils.download_zip_url(tags=[TEST_TAG], transformations=[{"width": 0.5}, {"width": 2.0}], cloud_name="demo")
-        six.assertRegex(self, result, r'^https://api.cloudinary.com/v1_1/demo/.*$')
+        upload_prefix = cloudinary.config().upload_prefix if cloudinary.config().upload_prefix else "https://api.cloudinary.com"
+        six.assertRegex(self, result, r'^{0}/v1_1/demo/.*$'.format(upload_prefix))
 
 
 if __name__ == '__main__':
