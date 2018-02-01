1f6018b9c656c16d4dad40229a3425de8add6c6f
Constantine Nathanson <constantine@cloudinary.com>
2018-02-01 18:26:32 +0200

    Add  `test_pre_save` for `CloudinaryField`
    Fix unit test `Choice` model compatibility with Django2
    
    (cherry picked from commit c6f4b36)

--- django_tests/test_cloudinaryField.py
+++ django_tests/test_cloudinaryField.py
@@ -1,21 +1,23 @@
+import os
+import unittest
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
 from urllib3.util import parse_url
 
 import cloudinary
 from cloudinary import CloudinaryResource, CloudinaryImage
-from django.test import TestCase
-
-from cloudinary.models import CloudinaryField
 from cloudinary.forms import CloudinaryFileField
-
+from cloudinary.models import CloudinaryField
 from .models import Poll
-
 from .test_helper import SUFFIX
 
 API_TEST_ID = "dj_test_{}".format(SUFFIX)
 
+TEST_IMAGE = "tests/logo.png"
+
 
 class TestCloudinaryField(TestCase):
-
     @classmethod
     def setUpTestData(cls):
         Poll.objects.create(question="with image", image="image/upload/v1234/{}.jpg".format(API_TEST_ID))
@@ -31,14 +33,24 @@ class TestCloudinaryField(TestCase):
         # Can't compare the objects, so compare url instead
         self.assertEqual(c.to_python('{}.jpg'.format(API_TEST_ID)).build_url(), res.build_url())
 
+    @unittest.skip("TODO: write test")
     def test_upload_options_with_filename(self):
         self.assertEqual(True, True)
 
+    @unittest.skip("TODO: write test")
     def test_upload_options(self):
         self.assertEqual(True, True)
 
     def test_pre_save(self):
-        self.assertEqual(True, True)
+        p = Poll()
+        with open(TEST_IMAGE, 'rb') as test_image_file:
+            p.image = SimpleUploadedFile(test_image_file.name, test_image_file.read())
+        c = CloudinaryField('image', width_field="image_width", height_field="image_height")
+        c.set_attributes_from_name('image')
+        prep_value = c.pre_save(p, None)
+        self.assertEqual(".png", os.path.splitext(prep_value)[1])
+        self.assertEqual(241, p.image_width)
+        self.assertEqual(51, p.image_height)
 
     def test_get_prep_value(self):
         c = CloudinaryField('image')
