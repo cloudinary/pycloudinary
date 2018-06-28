0390ddb88ce40a2a79cc789e818a375b65eda905
Constantine Nathanson <constantine@cloudinary.com>
2018-06-28 15:58:05 +0300

    Use exact match `=` instead of partial search `:` in `search_test`

--- tests/search_test.py
+++ tests/search_test.py
@@ -1,6 +1,6 @@
+import os
 import time
 import unittest
-import os
 
 from six import iterkeys
 from urllib3 import disable_warnings
@@ -11,13 +11,14 @@ from tests.test_helper import TEST_IMAGE, TEST_TAG, UNIQUE_TAG, SUFFIX
 
 disable_warnings()
 
-public_ids = ["api_test{0}_{1}".format(i, SUFFIX) for i in range(0, 3)]
-upload_results = ["++"]
-
 TEST_TAG = 'search_{}'.format(TEST_TAG)
 UNIQUE_TAG = 'search_{}'.format(UNIQUE_TAG)
 
+TEST_IMAGES_COUNT = 3
 MAX_INDEX_RETRIES = 10
+
+public_ids = ["api_test{0}_{1}".format(i, SUFFIX) for i in range(0, TEST_IMAGES_COUNT)]
+upload_results = ["++"]
 
 
 class SearchTest(unittest.TestCase):
@@ -26,18 +27,19 @@ class SearchTest(unittest.TestCase):
         cls.ready = False
         cloudinary.reset_config()
         if not cloudinary.config().api_secret: return
-        for id in public_ids:
+        for public_id in public_ids:
             res = uploader.upload(TEST_IMAGE,
-                            public_id=id,
-                            tags=[TEST_TAG, UNIQUE_TAG],
-                            context="stage=value", eager=[{"width": 100, "crop": "scale"}])
+                                  public_id=public_id,
+                                  tags=[TEST_TAG, UNIQUE_TAG],
+                                  context="stage=value",
+                                  eager=[{"width": 100, "crop": "scale"}])
             upload_results.append(res)
 
         attempt = 0
         while attempt < MAX_INDEX_RETRIES:
             time.sleep(1)
 
-            results = Search().expression("tags:{0}".format(UNIQUE_TAG)).execute()
+            results = Search().expression("tags={0}".format(UNIQUE_TAG)).execute()
 
             if len(results['resources']) == len(public_ids):
                 cls.ready = True
@@ -55,7 +57,6 @@ class SearchTest(unittest.TestCase):
             api.delete_resources_by_tag(UNIQUE_TAG)
         except Exception as e:
             logger.exception("Failed to delete test resources %s", e)
-            pass
 
     @unittest.skipUnless(cloudinary.config().api_secret, "requires api_key/api_secret")
     def test_should_create_empty_json(self):
@@ -106,8 +107,8 @@ class SearchTest(unittest.TestCase):
                      "Use env variable RUN_SEARCH_TESTS=1 if you really want to test it.")
     def test_should_return_all_images_tagged(self):
 
-        results = Search().expression("tags:{0}".format(UNIQUE_TAG)).execute()
-        self.assertEqual(len(results['resources']), 3)
+        results = Search().expression("tags={0}".format(UNIQUE_TAG)).execute()
+        self.assertEqual(len(results['resources']), TEST_IMAGES_COUNT)
 
     @unittest.skipUnless(cloudinary.config().api_secret, "requires api_key/api_secret")
     @unittest.skipIf(not os.environ.get('RUN_SEARCH_TESTS', False),
@@ -115,7 +116,7 @@ class SearchTest(unittest.TestCase):
                      "Use env variable RUN_SEARCH_TESTS=1 if you really want to test it.")
     def test_should_return_resource(self):
 
-        results = Search().expression("public_id:{0}".format(public_ids[0])).execute()
+        results = Search().expression("public_id={0}".format(public_ids[0])).execute()
         self.assertEqual(len(results['resources']), 1)
 
     @unittest.skipUnless(cloudinary.config().api_secret, "requires api_key/api_secret")
@@ -123,22 +124,23 @@ class SearchTest(unittest.TestCase):
                      "For this test to work, 'Advanced search' should be enabled for your cloud. " +
                      "Use env variable RUN_SEARCH_TESTS=1 if you really want to test it.")
     def test_should_paginate_resources_limited_by_tag_and_ordered_by_ascending_public_id(self):
-        results = Search().max_results(1).expression("tags:{0}".format(UNIQUE_TAG)).sort_by('public_id', 'asc').execute()
+        results = Search().max_results(1).expression("tags={0}".format(UNIQUE_TAG)).sort_by('public_id',
+                                                                                            'asc').execute()
         results = {'next_cursor': ''}
-        for i in range(0, 3): # get one resource at a time
-            results = Search()\
-                .max_results(1)\
-                .expression("tags:{0}".format(UNIQUE_TAG))\
-                .sort_by('public_id', 'asc')\
-                .next_cursor(results['next_cursor'])\
+        for i in range(0, TEST_IMAGES_COUNT):  # get one resource at a time
+            results = Search() \
+                .max_results(1) \
+                .expression("tags={0}".format(UNIQUE_TAG)) \
+                .sort_by('public_id', 'asc') \
+                .next_cursor(results['next_cursor']) \
                 .execute()
             self.assertEqual(len(results['resources']), 1)
             self.assertEqual(results['resources'][0]['public_id'],
-                             public_ids[i], 
+                             public_ids[i],
                              "{0} found public_id {1} instead of {2} ".format(i,
                                                                               results['resources'][0]['public_id'],
                                                                               public_ids[i]))
-            self.assertEqual(results['total_count'], 3)
+            self.assertEqual(results['total_count'], TEST_IMAGES_COUNT)
 
     @unittest.skipUnless(cloudinary.config().api_secret, "requires api_key/api_secret")
     @unittest.skipIf(not os.environ.get('RUN_SEARCH_TESTS', False),
@@ -146,8 +148,8 @@ class SearchTest(unittest.TestCase):
                      "Use env variable RUN_SEARCH_TESTS=1 if you really want to test it.")
     def test_should_include_context(self):
 
-        results = Search().expression("tags:{0}".format(UNIQUE_TAG)).with_field('context').execute()
-        self.assertEqual(len(results['resources']), 3)
+        results = Search().expression("tags={0}".format(UNIQUE_TAG)).with_field('context').execute()
+        self.assertEqual(len(results['resources']), TEST_IMAGES_COUNT)
         for res in results['resources']:
             self.assertEqual([key for key in iterkeys(res['context'])], [u'stage'])
 
@@ -157,13 +159,16 @@ class SearchTest(unittest.TestCase):
                      "Use env variable RUN_SEARCH_TESTS=1 if you really want to test it.")
     def test_should_include_context_tags_and_image_metadata(self):
 
-        results = Search().expression("tags:{0}".format(UNIQUE_TAG)).with_field('context').with_field('tags').with_field(
-            'image_metadata').execute()
-        self.assertEqual(len(results['resources']), 3)
+        results = Search().expression("tags={0}".format(UNIQUE_TAG)).with_field('context').with_field('tags').\
+            with_field('image_metadata').execute()
+
+        self.assertEqual(len(results['resources']), TEST_IMAGES_COUNT)
+
         for res in results['resources']:
             self.assertEqual([key for key in iterkeys(res['context'])], [u'stage'])
             self.assertTrue('image_metadata' in res)
             self.assertEqual(len(res['tags']), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
