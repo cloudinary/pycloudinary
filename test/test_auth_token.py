672d755a4a037dcb1332a1f103fdecec8eb9067f
Oleg Romanovskyi <me@olg.io>
2019-02-25 13:11:37 +0200

    Fix acl and url escaping in auth_token generation

--- test/test_auth_token.py
+++ test/test_auth_token.py
@@ -22,8 +22,8 @@ class AuthTokenTest(unittest.TestCase):
 
     def test_generate_with_start_time_and_duration(self):
         token = cloudinary.utils.generate_auth_token(acl='/image/*', start_time=1111111111, duration=300)
-        self.assertEqual('__cld_token__=st=1111111111~exp=1111111411~acl=%2fimage%2f%2a~hmac'
-                         '=0d5b0c9c1485ee162c459879fe62e06caa23bc26fec92d58bd100f2e1592eac6', token)
+        self.assertEqual('__cld_token__=st=1111111111~exp=1111111411~acl=%2fimage%2f*~hmac'
+                         '=1751370bcc6cfe9e03f30dd1a9722ba0f2cdca283fa3e6df3342a00a7528cc51', token)
 
     def test_should_add_token_if_authToken_is_globally_set_and_signed_is_True(self):
         cloudinary.config(private_cdn=True)
@@ -62,7 +62,7 @@ class AuthTokenTest(unittest.TestCase):
                                                  type="authenticated", transformation={"crop": "scale", "width": 300})
         self.assertEqual(url, "http://test123-res.cloudinary.com/image/authenticated/c_scale,"
                               "w_300/sample.jpg?__cld_token__=st=222222222~exp=222222322~hmac"
-                              "=7d276841d70c4ecbd0708275cd6a82e1f08e47838fbb0bceb2538e06ddfa3029")
+                              "=55cfe516530461213fe3b3606014533b1eca8ff60aeab79d1bb84c9322eebc1f")
 
     def test_should_compute_expiration_as_start_time_plus_duration(self):
         cloudinary.config(private_cdn=True)
@@ -80,8 +80,8 @@ class AuthTokenTest(unittest.TestCase):
         cookie_token = cloudinary.utils.generate_auth_token(**token_options)
         self.assertEqual(
             cookie_token,
-            "__cld_token__=st=222222222~exp=222222522~acl=%2f%2a%2ft_foobar~hmac="
-            "1284376353c1c43d6f6a98f2813c5596f4ff6f34d837cd853fd8c3c9e7f8428c"
+            "__cld_token__=st=222222222~exp=222222522~acl=%2f*%2ft_foobar~hmac="
+            "8e39600cc18cec339b21fe2b05fcb64b98de373355f8ce732c35710d8b10259f"
         )
 
     def test_should_ignore_url_if_acl_is_provided(self):
