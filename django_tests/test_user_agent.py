df84bcfcc0c0492cee3654fb679736b6bab2f99e
Constantine Nathanson <constantine@cloudinary.com>
2019-02-28 14:08:52 +0200

    Fix user-agent unit test assertion

--- django_tests/test_user_agent.py
+++ django_tests/test_user_agent.py
@@ -8,4 +8,4 @@ class TestUserAgent(TestCase):
     def test_django_user_agent(self):
         agent = cloudinary.get_user_agent()
 
-        six.assertRegex(self, agent, r'^Django\/\d\.\d+\.\d+ CloudinaryPython\/\d\.\d+\.\d+ \(Python \d\.\d+\.\d+\)$')
+        six.assertRegex(self, agent, r'^Django\/\d\.\d+\.?\d* CloudinaryPython\/\d\.\d+\.\d+ \(Python \d\.\d+\.\d+\)$')
