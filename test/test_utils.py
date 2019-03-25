6e91f90e7131bd7a0f7ccc141fa7099d72e7478c
idobarnoam <ido.barnoam@cloudinary.com>
2019-03-25 12:01:21 +0200

    Fix base64 data validation regex

--- test/test_utils.py
+++ test/test_utils.py
@@ -1054,6 +1054,9 @@ class TestUtils(unittest.TestCase):
             "https://cloudinary.com/images/old_logo.png",
             "s3://s3-us-west-2.amazonaws.com/cloudinary/images/old_logo.png",
             "gs://cloudinary/images/old_logo.png",
+            "data:image/gif;charset=utf8;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+            "data:image/gif;param1=value1;param2=value2;base64," +
+            "R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
             CL_BLANK
         ]
 
