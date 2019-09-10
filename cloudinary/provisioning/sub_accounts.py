1fba1499886acc1ebc6f9f8b004d5c75722430ea
Brian Luk <brian@cloudinary.com>
2019-09-10 17:34:55 +0300

    Python 2.7 compatability

--- cloudinary/provisioning/sub_accounts.py
+++ cloudinary/provisioning/sub_accounts.py
@@ -9,7 +9,8 @@ def sub_accounts(**options):
 
 def create_sub_account(name, **options):
     uri = ['sub_accounts']
-    return call_api("post", uri, {"name": name, **_only(options, "cloud_name", "base_sub_account_id", "description")}, **options)
+    return call_api("post", uri, dict(name=name,
+                                      **_only(options, "cloud_name", "base_sub_account_id", "description")), **options)
 
 
 def delete_sub_account(sub_account_id, **options):
@@ -24,4 +25,5 @@ def sub_account(sub_account_id, **options):
 
 def update_sub_account(sub_account_id, **options):
     uri = ['sub_accounts', sub_account_id]
-    return call_api("put", uri, _only(options, "cloud_name", "description", "base_sub_account_id", "enabled"), **options)
+    return call_api("put", uri,
+                    _only(options, "cloud_name", "description", "base_sub_account_id", "enabled"), **options)
