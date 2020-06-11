936a5bbbac39c97d484906e77a5f05e477e9760a
Constantine Nathanson <constantine@cloudinary.com>
2020-06-11 18:56:28 +0300

    Version 1.21.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from sys import version_info
 
 from setuptools import find_packages, setup
 
-version = "1.21.0"
+version = "1.21.1"
 
 with open('README.rst') as file:
     long_description = file.read()
