2fe9ea117ea9e7f5ace882689fb073f29decec95
Constantine Nathanson <constantine@cloudinary.com>
2020-11-22 21:22:10 -0500

    Fix indentation

--- test/helper_test.py
+++ test/helper_test.py
@@ -1,14 +1,14 @@
 # -*- coding: latin-1 -*-
 import json
-from contextlib import contextmanager
 import os
 import random
 import re
 import sys
 import time
+import traceback
+from contextlib import contextmanager
 from datetime import timedelta, tzinfo
 from functools import wraps
-import traceback
 
 import six
 from urllib3 import HTTPResponse
@@ -35,32 +35,30 @@ UNIQUE_TEST_FOLDER = UNIQUE_TAG + "_folder"
 
 ZERO = timedelta(0)
 
-EVAL_STR='if (resource_info["width"] < 450) { upload_options["tags"] = "a,b" }; ' \
-         'upload_options["context"] = "width=" + resource_info["width"]'
+EVAL_STR = 'if (resource_info["width"] < 450) { upload_options["tags"] = "a,b" }; ' \
+           'upload_options["context"] = "width=" + resource_info["width"]'
 
-ADDON_ALL = 'all'                                                   # Test all addons.
-ADDON_ASPOSE = 'aspose'                                             # Aspose Document Conversion.
-ADDON_AZURE = 'azure'                                               # Microsoft Azure Video Indexer.
-ADDON_BG_REMOVAL = 'bgremoval'                                      # Cloudinary AI Background Removal.
-ADDON_FACIAL_ATTRIBUTES_DETECTION = 'facialattributesdetection'     # Advanced Facial Attributes Detection.
-ADDON_GOOGLE = 'google'                                             # Google AI Video Moderation, Google AI
-                                                                    # Video Transcription, Google Auto Tagging,
-                                                                    # Google Automatic Video Tagging,
-                                                                    # Google Translation.
-ADDON_IMAGGA = 'imagga'                                             # Imagga Auto Tagging, Imagga Crop and Scale.
-ADDON_JPEGMINI = 'jpegmini'                                         # JPEGmini Image Optimization.
-ADDON_LIGHTROOM = 'lightroom'                                       # Adobe Photoshop Lightroom (BETA).
-ADDON_METADEFENDER = 'metadefender'                                 # MetaDefender Anti-Malware Protection.
-ADDON_NEURAL_ARTWORK = 'neuralartwork'                              # Neural Artwork Style Transfer.
-ADDON_OBJECT_AWARE_CROPPING = 'objectawarecropping'                 # Cloudinary Object-Aware Cropping.
-ADDON_OCR = 'ocr'                                                   # OCR Text Detection and Extraction.
-ADDON_PIXELZ = 'pixelz'                                             # Remove the Background.
-ADDON_REKOGNITION = 'rekognition'                                   # Amazon Rekognition AI Moderation,
-                                                                    # Amazon Rekognition Auto Tagging,
-                                                                    # Amazon Rekognition Celebrity Detection.
-ADDON_URL2PNG = 'url2png'                                           # URL2PNG Website Screenshots.
-ADDON_VIESUS = 'viesus'                                             # VIESUS Automatic Image Enhancement.
-ADDON_WEBPURIFY = 'webpurify'                                       # WebPurify Image Moderation.
+ADDON_ALL = 'all'  # Test all addons.
+ADDON_ASPOSE = 'aspose'  # Aspose Document Conversion.
+ADDON_AZURE = 'azure'  # Microsoft Azure Video Indexer.
+ADDON_BG_REMOVAL = 'bgremoval'  # Cloudinary AI Background Removal.
+ADDON_FACIAL_ATTRIBUTES_DETECTION = 'facialattributesdetection'  # Advanced Facial Attributes Detection.
+# Google AI Video Moderation, Google AI, Video Transcription, Google Auto Tagging, Google Automatic Video Tagging,
+# Google Translation.
+ADDON_GOOGLE = 'google'
+ADDON_IMAGGA = 'imagga'  # Imagga Auto Tagging, Imagga Crop and Scale.
+ADDON_JPEGMINI = 'jpegmini'  # JPEGmini Image Optimization.
+ADDON_LIGHTROOM = 'lightroom'  # Adobe Photoshop Lightroom (BETA).
+ADDON_METADEFENDER = 'metadefender'  # MetaDefender Anti-Malware Protection.
+ADDON_NEURAL_ARTWORK = 'neuralartwork'  # Neural Artwork Style Transfer.
+ADDON_OBJECT_AWARE_CROPPING = 'objectawarecropping'  # Cloudinary Object-Aware Cropping.
+ADDON_OCR = 'ocr'  # OCR Text Detection and Extraction.
+ADDON_PIXELZ = 'pixelz'  # Remove the Background.
+# Amazon Rekognition AI Moderation, Amazon Rekognition Auto Tagging, Amazon Rekognition Celebrity Detection.
+ADDON_REKOGNITION = 'rekognition'
+ADDON_URL2PNG = 'url2png'  # URL2PNG Website Screenshots.
+ADDON_VIESUS = 'viesus'  # VIESUS Automatic Image Enhancement.
+ADDON_WEBPURIFY = 'webpurify'  # WebPurify Image Moderation.
 
 
 class UTC(tzinfo):
@@ -166,6 +164,7 @@ def retry_assertion(num_tries=3, delay=3):
     :param num_tries: Number of tries to perform
     :param delay: Delay in seconds between retries
     """
+
     def retry_decorator(func):
         @wraps(func)
         def retry_func(*args, **kwargs):
