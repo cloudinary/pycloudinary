d5d6b7a79fbed51b9c6497af44f5c9148c6b4b6b
RTLcoil <oleg@rtl.co.il>
2020-12-31 10:07:07 +0200

    Extract addon type constants to a separate file

---
+++ test/addon_types.py
@@ -0,0 +1,21 @@
+ADDON_ALL = 'all'  # Test all addons.
+ADDON_ASPOSE = 'aspose'  # Aspose Document Conversion.
+ADDON_AZURE = 'azure'  # Microsoft Azure Video Indexer.
+ADDON_BG_REMOVAL = 'bgremoval'  # Cloudinary AI Background Removal.
+ADDON_FACIAL_ATTRIBUTES_DETECTION = 'facialattributesdetection'  # Advanced Facial Attributes Detection.
+# Google AI Video Moderation, Google AI, Video Transcription, Google Auto Tagging, Google Automatic Video Tagging,
+# Google Translation.
+ADDON_GOOGLE = 'google'
+ADDON_IMAGGA = 'imagga'  # Imagga Auto Tagging, Imagga Crop and Scale.
+ADDON_JPEGMINI = 'jpegmini'  # JPEGmini Image Optimization.
+ADDON_LIGHTROOM = 'lightroom'  # Adobe Photoshop Lightroom (BETA).
+ADDON_METADEFENDER = 'metadefender'  # MetaDefender Anti-Malware Protection.
+ADDON_NEURAL_ARTWORK = 'neuralartwork'  # Neural Artwork Style Transfer.
+ADDON_OBJECT_AWARE_CROPPING = 'objectawarecropping'  # Cloudinary Object-Aware Cropping.
+ADDON_OCR = 'ocr'  # OCR Text Detection and Extraction.
+ADDON_PIXELZ = 'pixelz'  # Remove the Background.
+# Amazon Rekognition AI Moderation, Amazon Rekognition Auto Tagging, Amazon Rekognition Celebrity Detection.
+ADDON_REKOGNITION = 'rekognition'
+ADDON_URL2PNG = 'url2png'  # URL2PNG Website Screenshots.
+ADDON_VIESUS = 'viesus'  # VIESUS Automatic Image Enhancement.
+ADDON_WEBPURIFY = 'webpurify'  # WebPurify Image Moderation.
