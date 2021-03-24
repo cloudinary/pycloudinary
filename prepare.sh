89aaba2b5dd5165b106c240633f5148656f9a088
Constantine Nathanson <constantine@cloudinary.com>
2021-03-24 01:32:13 -0400

    Fix `prepare.sh` script

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,14 @@
 /bin/rm -rf cloudinary/static
 mkdir -p cloudinary/static
 cd cloudinary/static
-curl -L https://github.com/cloudinary/cloudinary_js/tarball/master | tar zxvf - --strip=1 --exclude test '*/html' '*/js'
+
+OPTIONS=
+
+# GNU tar does not support wildcards by default, it needs explicit --wildcards option,
+# while BSD tar does not recognize this option
+if tar --version | grep -q 'gnu'; then
+    OPTIONS='--wildcards'
+fi
+
+curl -L https://github.com/cloudinary/cloudinary_js/tarball/master | tar zxvf - --strip=1 --exclude test $OPTIONS '*/html' '*/js'
 
