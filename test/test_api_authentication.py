4ff0842de17ba501b4579a3f018ff27089104d55
Constantine Nathanson <constantine@cloudinary.com>
2021-05-26 15:14:37 +0300

    Remove redundant imports

--- test/test_api_authentication.py
+++ test/test_api_authentication.py
@@ -1,4 +1,3 @@
-import base64
 import unittest
 
 import six
@@ -7,12 +6,10 @@ from mock import patch
 import cloudinary
 from cloudinary import api
 from cloudinary import uploader
-from test.helper_test import UNIQUE_TEST_ID, TEST_IMAGE, get_headers, get_params
+from test.helper_test import TEST_IMAGE, get_headers, get_params
 from test.test_api import MOCK_RESPONSE
 from test.test_config import OAUTH_TOKEN, CLOUD_NAME, API_KEY, API_SECRET
 from test.test_uploader import API_TEST_PRESET
-
-UNIQUE_IMAGE_PUBLIC_ID = 'asset_image_{}'.format(UNIQUE_TEST_ID)
 
 
 class ApiAuthenticationTest(unittest.TestCase):
