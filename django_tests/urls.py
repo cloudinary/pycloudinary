d7bc039062bfa941a91b9a00c9dd49a87505cacf
Constantine Nathanson <constantine@cloudinary.com>
2022-01-13 10:54:06 -0500

    Add support for Django 4

--- django_tests/urls.py
+++ django_tests/urls.py
@@ -1,4 +1,9 @@
-from django.conf.urls import url
+import django
+
+if int(django.__version__[0]) > 3:
+    from django.urls import re_path as url
+else:
+    from django.conf.urls import url
 
 from .views import index
 
