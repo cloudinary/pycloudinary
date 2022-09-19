a1ec216650ce2ecc3d6a82a0f4f970537e47091a
me <me@me.com>
2022-09-18 18:19:58 -0700

    add django user-agent test

--- django_tests/test_user_agent.py
+++ django_tests/test_user_agent.py
@@ -2,10 +2,14 @@ import six
 from django.test import TestCase
 
 import cloudinary
+from platform import platform as pf
 
 
 class TestUserAgent(TestCase):
     def test_django_user_agent(self):
         agent = cloudinary.get_user_agent()
 
-        six.assertRegex(self, agent, r'^Django\/\d\.\d+\.?\d* CloudinaryPython\/\d\.\d+\.\d+ \(Python \d\.\d+\.\d+\)$')
+        os_info = pf().lower()
+        os_info = os_info.split("-")
+
+        six.assertRegex(self, agent, r'^CloudinaryPython\/\d\.\d+\.\d+ \(Python \d\.\d+\.\d+\/{}\)$'.format(os_info[0].capitalize()))
