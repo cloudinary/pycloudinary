3147d93d694b3164b8de65afb19d89c579148260
Avinash Sharma <96874114+Avinash238@users.noreply.github.com>
2023-10-02 20:32:15 +0530

    Update models.py
    Reordered fields for better readability, placing related fields closer together.

--- django_tests/models.py
+++ django_tests/models.py
@@ -1,3 +1,4 @@
+'''
 from six import python_2_unicode_compatible
 
 from cloudinary.models import CloudinaryField
@@ -25,3 +26,28 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice.encode()
+'''
+from six import python_2_unicode_compatible
+from cloudinary.models import CloudinaryField
+from django.db import models
+
+@python_2_unicode_compatible
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    image = CloudinaryField('image', null=True, width_field='image_width', height_field='image_height')
+    image_width = models.PositiveIntegerField(null=True)
+    image_height = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return self.question
+
+@python_2_unicode_compatible
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
+
+    def __str__(self):
+        return self.choice
+
+
