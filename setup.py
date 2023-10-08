1f336c263c00ffb29a7a2234dc9c7a8761a20f63
Nico Mexis <nico.mexis@kabelmail.de>
2023-10-08 23:48:11 +0200

    Readd test stuff

--- setup.py
+++ setup.py
@@ -1,2 +1,10 @@
+from sys import version_info
+
 import setuptools
-setuptools.setup()
+
+setuptools.setup(
+    test_suite="test",
+    tests_require=[
+        "mock" + ("<4" if version_info < (3, 6) else "")
+    ],
+)
