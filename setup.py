79de139d66218806b3c9432049363ead4c0d42fa
Nico Mexis <nico.mexis@kabelmail.de>
2023-10-09 20:51:46 +0200

    Migrate beta entries back to setup.py approach

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from sys import version_info
 
-import setuptools
+from setuptools import find_packages, setup
 
-setuptools.setup(
+setup(
+    packages=find_packages(exclude=['ez_setup', 'examples', 'test', 'django_tests', 'django_tests.*']),
+    include_package_data=True,
+    zip_safe=False,
     test_suite="test",
     tests_require=[
         "mock" + ("<4" if version_info < (3, 6) else "")
