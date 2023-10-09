30d865d854e04f95d6ec95480f636f22923f5a65
Nico Mexis <nico.mexis@kabelmail.de>
2023-10-09 21:13:35 +0200

    Attempts to fix Python 2.7

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from sys import version_info
 
 from setuptools import find_packages, setup
 
-setup(
-    packages=find_packages(exclude=['ez_setup', 'examples', 'test', 'django_tests', 'django_tests.*']),
-    include_package_data=True,
-    zip_safe=False,
-    test_suite="test",
-    tests_require=[
-        "mock" + ("<4" if version_info < (3, 6) else "")
-    ],
-)
+setup(packages=find_packages(exclude=['ez_setup', 'examples', 'test', 'django_tests', 'django_tests.*']),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite="test",
+      install_requires=[
+          "six",
+          "urllib3>=1.26.5",
+          "certifi"
+      ],
+      tests_require=[
+          "mock" + ("<4" if version_info < (3, 6) else "")
+      ],
+      )
